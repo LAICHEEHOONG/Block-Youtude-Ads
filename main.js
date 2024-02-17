c2d0ebabd8dd6f97a43e4721f3a52a692454243e
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2024-02-17 21:18:16 +0800

    record time

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const handle = {
   counter: {
     skip: 0,
     duration: 0,
+    time: "",
   },
   detectAds: () => {
     const adShowing = document.querySelector(".ad-showing");
@@ -16,6 +17,7 @@ const handle = {
     if (video.currentTime) {
       video.currentTime = video.duration;
       handle.counter.duration++;
+      handle.recordTime();
       handle.message();
     }
   },
@@ -23,11 +25,24 @@ const handle = {
     const skipBtn = document.querySelector(".ytp-ad-skip-button-modern");
     skipBtn.click();
     handle.counter.skip++;
+    handle.recordTime();
     handle.message();
   },
   message: () => {
     console.log(`跳转广告: ${handle.counter.duration} 次`);
     console.log(`点击跳过: ${handle.counter.skip} 次`);
+    console.log(`时间: ${handle.counter.time}`);
+  },
+  recordTime: () => {
+    const currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let amPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let formattedTime = hours + ":" + minutes + amPm;
+    handle.counter.time = formattedTime;
   },
 };
 

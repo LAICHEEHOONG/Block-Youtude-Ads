0051eeeef43fe0eeafe888ca76bf0179cb3096b6
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2024-02-17 15:13:14 +0800

    fix bug skip not work

--- main.js
+++ main.js
@@ -2,28 +2,24 @@ const blockAds = () => {
   const video = document.querySelector("video");
   let blocked = 0;
 
-  const handleSkip = () => {
+  const handleSkip = async () => {
     const skipBtn = document.querySelector(".ytp-ad-skip-button");
     if (skipBtn) {
       skipBtn.click();
     }
   };
 
-  const videoDuration = () => {
+  const videoDuration = async () => {
     const adShowing = document.querySelector(".ad-showing");
     if (adShowing && video.currentTime) {
       video.currentTime = video.duration;
       blocked++;
       console.log(`屏蔽的广告数量: ${blocked}`);
-      handleSkip();
+      await handleSkip();
     }
   };
 
-  const handleTimeUpdate = () => {
-    videoDuration();
-  };
-
-  video.addEventListener("timeupdate", handleTimeUpdate);
+  video.addEventListener("timeupdate", videoDuration);
 };
 
 blockAds();
